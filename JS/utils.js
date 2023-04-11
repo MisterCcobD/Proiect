d48ef07eab8c27243bbad23638e8d253308605a1
MisterCcobD <126014590+MisterCcobD@users.noreply.github.com>
2023-04-11 18:35:42 +0300

    Adaugari modificari (#5)
    * th in tr la pagini produse
    
    * bugg nav
    
    * bugg nav
    
    * rezolvat pagini servicii
    
    * ceva
    
    * TODO
    
    * pag echipamente footer si gap form
    
    * idee, m/header::before
    
    * footer img{cursor pointer}
    
    * locatie a:hover transform scale 1.2
    
    * is showHeader modificat si todo
    
    * nav2 left 100% transform translate left 10%
    nav img flex basis & width 30% =>4both navs
    
    * TODO
    
    * functie scrollToForm ciordita dar inteleasa
    
    * product media despre query font sizes
    
    * nav Z-INDEX 999
    
    * header media query la 1124px
    
    * TODO
    
    * pag echipamente img separate cu width 50%/m100% max-height 400/m250 si border-radious /d
    
    * #preturi /d img si tabel la nivel cu si overflow x
    nav T & C
    
    * ::-webkit-scrollbar
    
    * facut rost de o functie care sa deschida pagina t&C.html
    
    * sters inline css si inline js & modificat nume funtie
    
    * TERMINAT TOATE PAGINILE

--- JS/utils.js
+++ JS/utils.js
@@ -1,7 +1,21 @@
-
 function goToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+
+function scrollToForm() {
+  const form = document.getElementById("form");
+  if (form) {
     window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+      top: form.offsetTop,
+      behavior: "smooth"
     });
   }
+}
+function showTNS() {
+  window.open("T&C.html", "newWindow", "width=400,height=400");
+}
+
